fd6fbcbbf69be9b45cddba258dc26afa449ca985
Arthur Gonze Machado <arthur.gonze@ice.ufjf.br>
2021-07-15 18:23:43 -0300

    testing touch detection

--- T2/t2_ex02.js
+++ T2/t2_ex02.js
@@ -8,11 +8,11 @@ AFRAME.registerComponent('cursor-listener', {
     var aim = document.querySelector("#aim");
     var direction = new THREE.Vector3();
 
+    
     sceneEl.addEventListener('click', function (evt) {
       if(evt && evt.detail && evt.detail.intersection && evt.detail.intersection.point)
       {
-        lastIndex = (lastIndex + 1) % COLORS.length;
-        aim.setAttribute('material', 'color', COLORS[lastIndex]);
+        aim.setAttribute('material', 'color', COLORS[0]);
         // console.log('I was clicked at: ', evt.detail.intersection.point);
 
         // get the cameras world direction
@@ -31,8 +31,27 @@ AFRAME.registerComponent('cursor-listener', {
     sceneEl.addEventListener('touchstart', function (evt) {
       if(evt && evt.detail && evt.detail.intersection && evt.detail.intersection.point)
       {
-        lastIndex = (lastIndex + 1) % COLORS.length;
-        aim.setAttribute('material', 'color', COLORS[lastIndex]);
+        aim.setAttribute('material', 'color', COLORS[1]);
+        // console.log('I was clicked at: ', evt.detail.intersection.point);
+
+        // get the cameras world direction
+        sceneEl.camera.getWorldDirection(direction);
+        // multiply the direction by a "speed" factor
+        direction.multiplyScalar(0.1);
+        // get the current position
+        var pos = player.object3D.position;
+        // add the direction vector
+        pos.add(direction);
+        // set the new position
+        player.object3D.position.add(direction)
+      }
+    });
+
+    
+    sceneEl.addEventListener('buttondown', function (evt) {
+      if(evt && evt.detail && evt.detail.intersection && evt.detail.intersection.point)
+      {
+        aim.setAttribute('material', 'color', COLORS[2]);
         // console.log('I was clicked at: ', evt.detail.intersection.point);
 
         // get the cameras world direction
