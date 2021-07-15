6a55632c7c12b27fe0468498ef5ab9bb689fe5ec
Arthur Gonze Machado <arthur.gonze@ice.ufjf.br>
2021-07-15 18:07:17 -0300

    T2 ex02 forward movement

--- T2/t2_ex02.js
+++ T2/t2_ex02.js
@@ -1,11 +1,41 @@
+AFRAME.registerComponent('cursor-listener', {
+  init: function () {
+    var sceneEl = document.querySelector('a-scene');
+    var lastIndex = -1;
+    var COLORS = ['red', 'green', 'blue'];
+
+    var player = document.querySelector("a-camera");
+    var aim = document.querySelector("#aim");
+    var direction = new THREE.Vector3();
+
+    sceneEl.addEventListener('click', function (evt) {
+      if(evt && evt.detail && evt.detail.intersection && evt.detail.intersection.point)
+      {
+        lastIndex = (lastIndex + 1) % COLORS.length;
+        aim.setAttribute('material', 'color', COLORS[lastIndex]);
+        // console.log('I was clicked at: ', evt.detail.intersection.point);
+
+        // get the cameras world direction
+        sceneEl.camera.getWorldDirection(direction);
+        // multiply the direction by a "speed" factor
+        direction.multiplyScalar(0.1);
+        // get the current position
+        var pos = player.object3D.position;
+        // add the direction vector
+        pos.add(direction);
+        // set the new position
+        player.object3D.position.add(direction)
+      }
+    });
+  }
+});
 
 AFRAME.registerComponent('ambiente-2', {
   init: function () {
-    
     // Solution for Getting Entities.
-    var sceneEl = document.querySelector('a-scene');  // Or this.el since we're in a component.
+    var sceneEl = document.querySelector('a-scene'); 
     console.log(sceneEl);
-    
+
     for (var i = 0; i < 50; i++) {
       var xPos = Math.random() * 50-25;
       var zPos = Math.random() * 50-25;
